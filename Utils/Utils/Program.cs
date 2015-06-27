d47337ba90fe6c215773dde62c8640d99bd49514
liranmotol <liranmotola@gmail.com>
2015-06-27 09:35:04 +0300

    utils

---
+++ Utils/Utils/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ReadCSV();
+        }
+
+        private static void ReadCSV()
+        {
+            var reader = new StreamReader(File.OpenRead(@"D:\apps\Mobile\PmupApp\Data\2014_2015_RegForms.csv"));
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                listA.Add(values[0]);
+                listB.Add(values[1]);
+            }
+        }
+    }
+}
---
+++ Utils/Utils/StudentsModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+   public class StudentsModel
+    {
+public string       studentID;
+public string             img;
+public string             firstName;
+public string             lastName;
+public string        grade; 
+public string       sClass;
+public string       pickUpFrom;
+public string       healthIssues;
+public CeckedInOutModel  checkedIn;
+public CeckedInOutModel pickUp;
+public List<string>  pickUpOptions;
+public bool        isPickedUp;
+public  string      parent1Name;
+public  string      parent1Num;
+public  string      parent2Name;
+public  string      parent2Num;
+
+    }
+
+public class CeckedInOutModel
+{
+ public  string   byWhom;
+ public string when;
+}
+
+
+
+}
