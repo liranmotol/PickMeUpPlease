0a5a01c191d96a25a0fdaba7093489144864a115
liranmotol <liranmotola@gmail.com>
2015-08-16 00:32:06 +0300

    auth

--- Server/Server/Controllers/AccountController.cs
+++ Server/Server/Controllers/AccountController.cs
@@ -7,9 +7,11 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Server.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     [RoutePrefix("Account")]
     public class AccountController : ApiController
     {
