4aedf79409bd3d1721078cbdbb469ea8a60aedad
liranmotol <liranmotola@gmail.com>
2015-12-02 23:15:42 +0200

    ManagePickUps

---
+++ ClientNew/MobileApplication/Controllers/GeneralController.cs
@@ -0,0 +1,45 @@
+﻿using MobileApplication.Models;
+using MobileApplication.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileApplication.Controllers
+{
+    [Authorize]
+
+    public class GeneralController : Controller
+    {
+        public ActionResult ShowPickUps()
+        {
+            ClassPickUpModel.GetInstance();
+            return View(ClassPickUpModel.PickUpDic);
+        }
+
+        public ActionResult ManagePickUps()
+        {
+            ClassPickUpModel.GetInstance();
+            return View(ClassPickUpModel.PickUpDic);
+        }
+
+        public void UpdateShowPickUps(string Who,string Where)
+        {
+            if (string.IsNullOrEmpty(Who))
+                return;
+            if (ClassPickUpModel.PickUpDic.Any(c => c.Key.ToUpper() == Who.ToUpper()))
+                ClassPickUpModel.PickUpDic[Who] = Where;
+            else
+                ClassPickUpModel.PickUpDic.Add(Who.ToUpper(), Where);
+        }
+        public void RemoveValue(string Who)
+        {
+            if (Who==null)
+                return;
+            if (ClassPickUpModel.PickUpDic.Any(c => c.Key.ToUpper() == Who.ToUpper()))
+                ClassPickUpModel.PickUpDic.Remove(Who);
+        }
+
+    }
+}
---
+++ ClientNew/MobileApplication/Models/ClassPickUpModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileApplication.Models
+{
+    public class ClassPickUpModel
+    {
+        public static Dictionary<string, string> PickUpDic;
+
+        private static ClassPickUpModel _instance = null;
+        internal static ClassPickUpModel GetInstance()
+        {
+            if (_instance == null)
+                _instance = new ClassPickUpModel();
+            return _instance;
+        }
+
+        public ClassPickUpModel()
+        {
+            PickUpDic = new Dictionary<string, string>();
+            //PickUpDic .Add("Class", "Where" );
+        }
+    }
+}
